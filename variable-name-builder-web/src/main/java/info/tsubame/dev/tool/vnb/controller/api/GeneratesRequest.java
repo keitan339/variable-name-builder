457a4cd0c2b300c38cf4f9d3118c6ce8ec1fca28
KeitaSasaki <keitan339@gmail.com>
2019-12-21 19:09:05 +0900

    spotbugsを追加

--- variable-name-builder-web/src/main/java/info/tsubame/dev/tool/vnb/controller/api/GeneratesRequest.java
+++ variable-name-builder-web/src/main/java/info/tsubame/dev/tool/vnb/controller/api/GeneratesRequest.java
@@ -1,8 +1,10 @@
 package info.tsubame.dev.tool.vnb.controller.api;
 
+import info.tsubame.dev.tool.vnb.SuppressFBWarnings;
 import lombok.Data;
 
 @Data
+@SuppressFBWarnings(value = {"EI_EXPOSE_REP", "EI_EXPOSE_REP"}, justification = "for generation by lombok")
 public class GeneratesRequest {
 
     private String[] texts;
